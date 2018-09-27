f0628614bd9ee98949e90c6d6d33584f72c78b70
Nguyen Anh <anhnd.hust@gmail.com>
2018-09-27 16:43:33 +0700

    Init project

---
+++ Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using FileStorage.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileStorage.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbContextOptions<DataContext> Options { get; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            Options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().ToTable("Country");
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
